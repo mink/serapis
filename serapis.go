c4b7ffcc077414096a51b9677198f9448b6b1e50
Zac Olden <zacolden@gmail.com>
2021-07-25 00:00:58 +1000

    Add basic TCP server & connection handler

--- serapis.go
+++ serapis.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"net/http"
 
 	"github.com/prometheus/client_golang/prometheus/promhttp"
@@ -10,6 +11,37 @@ import (
 func main() {
 	fmt.Println("Serapis")
 
+	go metrics()
+	serve()
+}
+
+func metrics() {
 	http.Handle("/metrics", promhttp.Handler())
 	http.ListenAndServe(":2112", nil)
+}
+
+func serve() {
+	listener, err := net.Listen("tcp", ":1337")
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			panic(err)
+		}
+
+		go handle(conn)
+	}
+}
+
+func handle(conn net.Conn) {
+	buffer := make([]byte, 1024)
+	n, err := conn.Read(buffer)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%d bytes received: %s\n", n, buffer)
 }
