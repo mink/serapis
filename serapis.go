2c0eb4750701e033c01249b846044a768e887952
Zac Olden <zacolden@gmail.com>
2021-07-24 23:44:36 +1000

    Prometheus & Grafana installation

--- serapis.go
+++ serapis.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
 
 func main() {
 	fmt.Println("Serapis")
+
+	http.Handle("/metrics", promhttp.Handler())
+	http.ListenAndServe(":2112", nil)
 }
